6dfa246926b02b0c57adf589df8f16104762f625
hadi khir <khir.hadi@gmail.com>
2024-12-03 16:47:34 -0400

    updating app sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,21 +1,29 @@
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarHeader,
-  } from "@/components/ui/sidebar"
-  
-  export function AppSidebar() {
-    return (
-      <Sidebar>
-        <SidebarHeader />
-        <SidebarContent>
-          <SidebarGroup />
-          <SidebarGroup />
-        </SidebarContent>
-        <SidebarFooter />
-      </Sidebar>
-    )
-  }
-  
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+import Image from "next/image"
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader className="bg-stone-900">
+        <Image
+          className="text-right"
+          src={'/athletic_logo.png'}
+          alt="Company Logo"
+          width={150}
+          height={150}
+        />
+      </SidebarHeader>
+      <SidebarContent className="bg-stone-900">
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
