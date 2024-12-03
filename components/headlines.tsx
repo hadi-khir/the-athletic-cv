73a476d25677d3bc62085c9cf32616d5d40cfa53
Hadi Khir <khir.hadi@gmail.com>
2024-12-03 01:58:24 +0000

    second pass on background components

---
+++ components/headlines.tsx
@@ -0,0 +1,32 @@
+interface Headline {
+        id: number,
+        text: string
+}
+
+interface HeadlinePropType {
+    headlines: Headline[]
+}
+
+const Headlines = ({headlines}: HeadlinePropType) => {
+
+
+    return (
+      <div id='headlines' className='hidden md:block' >
+      <h2 className='font-bold text-md uppercase mb-4'>Headlines</h2>
+      <ul className='space-y-3'>
+        {headlines.map((line) => (
+          <li
+            key={line.id}
+            className='flex items-start space-x-2 text-md text-gray-800'
+          >
+            <span className='w-1 h-1 bg-black rounded-full mt-2'></span>
+            <p>{line.text}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+    )
+
+}
+
+export default Headlines;
